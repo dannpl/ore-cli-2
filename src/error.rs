a798f5f8f2249bd3dc7ac75cea916a6fa3331f10
dannpl <dancardosobr@gmail.com>
2024-09-17 17:54:52 -0300

    remove string

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,6 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("{0}")]
-    Internal(String),
-    #[error("reqwest")]
-    Reqwest(#[from] reqwest::Error),
-    #[error("solana rpc client")]
-    SolanaRpcClient(#[from] solana_client::client_error::ClientError),
-    #[error("solana program")]
-    SolanaProgram(#[from] solana_program::program_error::ProgramError),
+    #[error("reqwest")] Reqwest(#[from] reqwest::Error),
+    #[error("solana rpc client")] SolanaRpcClient(#[from] solana_client::client_error::ClientError),
+    #[error("solana program")] SolanaProgram(#[from] solana_program::program_error::ProgramError),
 }
